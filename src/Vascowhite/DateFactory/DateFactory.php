f81f793c1a31c7cae7efd6607195e7f7fa6e59ff
Paul White <paul@vascowhite.co.uk>
2014-12-01 17:13:55 +0300

    Finally, a fully working and tested version

---
+++ src/Vascowhite/DateFactory/DateFactory.php
@@ -0,0 +1,96 @@
+<?php
+/*
+    Copyright (C) 2014  Paul White
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * User: Paul White
+ * Date: 01/12/2014
+ * 
+ * File: DateFactory.php
+ * @package datefactory
+ */
+ 
+ /**
+  * @package 
+  */
+
+namespace Vascowhite\DateFactory;
+
+class DateFactory
+{
+    /**
+     * @param null || String $date A valid date string in the format Y-m-d H:i:s
+     * @param null || String $timezone A valid TimeZone string from this list http://php.net/manual/en/timezones.php
+     * @throws InvalidArgumentException
+     *
+     * @return DateTime
+     */
+
+    const FORMAT = 'Y-m-d H:i:s';
+
+    public static function getDate($dateString = null, $timezone = null)
+    {
+        if(!$timezone){
+            $tz = new \DateTimeZone(date_default_timezone_get());
+        } else {
+            if(!self::checkTZStingIsValid($timezone)){
+                throw new \InvalidArgumentException('Invalid TimeZone string passed');
+            }
+            $tz = new \DateTimeZone($timezone);
+        }
+
+        if(!$dateString){
+            $date = new \DateTime(null, $tz);
+        } else {
+            $dateString = self::fixTime($dateString);
+
+            list($year, $month, $day) = explode('-', explode(' ', $dateString)[0]);
+
+            if(checkdate((int)$month, (int)$day, (int)$year)){
+                $date = \DateTime::createFromFormat(self::FORMAT, $dateString, $tz);
+            } else {
+                throw new \InvalidArgumentException('Invalid date string passed');
+            }
+        }
+        return $date;
+    }
+
+    /**
+     * @param String $TZString
+     * @return bool
+     */
+    private static function checkTZStingIsValid($TZString)
+    {
+        return in_array($TZString, \DateTimeZone::listIdentifiers());
+    }
+
+    /**
+     * @param String $dateString
+     * @return string
+     */
+    private static function fixTime($dateString)
+    {
+        $split = explode(' ', $dateString);
+        if(!isset($split[1])){
+            return $dateString . ' 00:00:00';
+        }
+        if(count(explode(':', $split[1])) < 3){
+            return $dateString . ':00';
+        }
+        return $dateString;
+    }
+}
---
+++ src/Vascowhite/Tests/DateFactoryTest.php
@@ -0,0 +1,81 @@
+<?php
+/*
+    Copyright (C) 2014  Paul White
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * User: Paul White
+ * Date: 01/12/2014
+ * 
+ * File: DateFactoryTest.php
+ * @package datefactory
+ */
+ 
+ /**
+  * @package 
+  */
+
+namespace Vascowhite\DateFactory\Tests;
+use Vascowhite\DateFactory\DateFactory;
+
+class DateFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCanGetDateWithNullParameters()
+    {
+        $testDate = DateFactory::getDate();
+        $this->assertInstanceOf('\DateTime', $testDate);
+    }
+
+    public function testCanGetDateWithNoTimeZone()
+    {
+        $testDate = DateFactory::getDate('2014-12-25 12:00:00');
+        $this->assertInstanceOf('\DateTime', $testDate);
+    }
+
+    public function testCanGetDateWithValidTimeZone()
+    {
+        $testDate = DateFactory::getDate('2014-12-25 12:00:00', 'Europe/London');
+        $this->assertInstanceOf('\DateTime', $testDate);
+    }
+
+    public function testCanGetDateWithIncompleteTime()
+    {
+        $testDate = DateFactory::getDate('2014-12-25', 'Europe/London');
+        $this->assertInstanceOf('\DateTime', $testDate);
+
+        $testDate = DateFactory::getDate('2014-12-25 12:00', 'Europe/London');
+        $this->assertInstanceOf('\DateTime', $testDate);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Invalid date string passed
+     */
+    public function testWillThrowExceptionsForInvalidDateString()
+    {
+        $testDate = DateFactory::getDate('2014-13-25');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Invalid TimeZone string passed
+     */
+    public function testWillThrowExceptionsForInvalidTimeZoneString()
+    {
+        $testDate = DateFactory::getDate(null, 'invalidtimezonestring');
+    }
+}
+ 
